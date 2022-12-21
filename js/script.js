d366285b973f8b1d782336ce2f8bdab1a9078a8c
Davide Di Girolamo <davide.digi2001@gmail.com>
2022-12-21 18:06:45 +0100

    finito ciclo for bonus

--- js/script.js
+++ js/script.js
@@ -10,26 +10,20 @@ Come faccio a sapere se un numero è divisibile per un altro? Abbiamo visto qual
 Consigli del giorno:
 Scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
 Proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
-
-BONUS 1:
-Crea un container nel DOM , aggiungendo (attraverso la funzione append()) un elemento html con il numero o la stringa corretta da mostrare.
-BONUS 2:
-
-Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz. Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna.
 */
 
 for (let i = 1; i <= 100; i++) {
     
     if (i % 3 == 0 && i % 5 == 0) {
-        console.log("FizzBuzz")
+        console.log("FizzBuzz");
     }
     else if (i % 3 == 0) {
-        console.log("Fizz")
+        console.log("Fizz");
     }
     else if (i % 5 == 0) {
-        console.log("Buzz")
+        console.log("Buzz");
     }
     else {
-        console.log(i)
+        console.log(i);
     }
 }
